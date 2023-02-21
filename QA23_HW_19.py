3360239e445b93be47cfac65fb9a628e13983f32
prokopenkoga1977 <prokopenkoga1977@gmail.com>
2023-02-21 11:34:36 +0200

    Added file QA23_HW_19

---
+++ QA23_HW_19.py
@@ -0,0 +1,17 @@
+user = {
+    "name": "John",
+    "age": 23,
+    "friends": [
+        "Mike", "Bob", "Joe"
+    ],
+    "skills": ("HTML", "CSS", "JS"),
+}
+
+# 1) Find Mike into the user , than if u found Mike create a variable for him
+# 2) Print Mike at the console
+# 3) Get age of user , and guess when he was born
+# 4) Skills must look as : Python , QA , Selenium
+# 5) Sort john's friends
+# 6) After sort find index position of "Bob"
+# 7) Add new friends to friends : "Taras" , "Danya" , "Bidden"
+# 8) Show how much skills user has
